28a6ff6de73a7b35fe0c9c725dca35cd473be7ac
luisgiltajamar <luis.gil@tajamar365.com>
2014-12-15 12:38:52 +0100

    a

---
+++ Correo/Controllers/CorreosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Correo.Controllers
+{
+    public class CorreosController : Controller
+    {
+        // GET: Correos
+        public ActionResult Index()
+        {
+            return View(new Models.Correo());
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(Models.Correo correo)
+        {
+            var copia = ConfigurationManager.
+                AppSettings["EnviarCopia"].ToString();
+            var smtp = new SmtpClient();
+            
+            var msg = new MailMessage();
+            msg.Subject = correo.Asunto;
+            msg.To.Add(new MailAddress(correo.Destino));
+            
+            if(copia=="Si")
+                msg.Bcc.Add(new MailAddress("ftajamar2@tajamar365.com"));
+            
+            msg.Body = correo.Mensaje;
+            msg.IsBodyHtml = true;
+            try
+            {
+                smtp.Send(msg);
+            }
+            catch (Exception ee)
+            {
+                var e = "";
+            }
+            return View();
+        }
+    }
+}
---
+++ Correo/Models/Correo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Correo.Models
+{
+    public class Correo
+    {
+        public String Asunto { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public String Destino { get; set; }
+        public String Mensaje { get; set; }
+    }
+}
